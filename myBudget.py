17f1a5e053966502d08e8c6d12539138f7568cb8
unknown <lindseyjrobertson@gmail.com>
2024-07-14 22:59:21 -0400

    reset date in header, added additional comments

--- myBudget.py
+++ myBudget.py
@@ -1,7 +1,7 @@
 """
 
 Author:  Lindsey Robertson
-Date written: 07/14/24
+Date written: 07/13/24
 Assignment:   Final Project
 Short Desc:   This program is a budget tracker, allowing a user to track how much they have spent for the month. 
 
@@ -12,10 +12,11 @@ from tkinter import *
 #create root widget
 root = Tk()
 
+#the following section was done following the tutorial video for frames to get the project started. The final project will include panels within the frame and the frame attributes will be changed.
 #create a window
     #can add a text attribute if desired, padding here is inside of frame
 mainFrame = LabelFrame(root, padx = 50, pady = 50)
-#padding here is inside the frame
+#padding here is outside the frame
 mainFrame.pack(padx = 100, pady = 100)
 #create a label widget
 mainLabel = Label(mainFrame, text = "My Budget")
@@ -27,7 +28,7 @@ mainLabel.grid(row = 0, column = 0)
 root.mainloop()
 
 # def main():
-#     BudgetTracker().mainloop()
+#     myBudget().mainloop()
 
 # if __name__ == "__main__" :
 #     main()
